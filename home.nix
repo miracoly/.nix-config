6a27832e0013086994e8689542b215271b506fe9
miracoly <68049792+miracoly@users.noreply.github.com>
2024-03-03 20:18:07 +0100

    fix(home): make nm-applet appear

--- home.nix
+++ home.nix
@@ -619,6 +619,13 @@
     xautolock.enable = true;
   };
 
+  systemd.user.targets.tray = {
+		Unit = {
+			Description = "Home Manager System Tray";
+			Requires = [ "graphical-session-pre.target" ];
+		};
+	};
+
   # i3
   xsession.windowManager.i3 = {
     enable = true;
