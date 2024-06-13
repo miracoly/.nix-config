486d36099ab4d159890a6a5ccd80c8bfb4a28910
miracoly <68049792+miracoly@users.noreply.github.com>
2024-06-13 20:44:10 +0200

    chore(home): update codecrafters cli

--- derivations/codecrafters-cli.nix
+++ derivations/codecrafters-cli.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl }:
 stdenv.mkDerivation rec {
   name = "codecrafters-cli-${version}";
-  version = "25";
+  version = "29";
 
   # https://github.com/codecrafters-io/cli/releases
   src = fetchurl {
     url =
-      "https://github.com/codecrafters-io/cli/releases/download/v25/v25_linux_amd64.tar.gz";
-    sha256 = "9df1ad971cf337325397443ab45be6c0514661d1adb0d8c713f172b83224c261";
+      "https://github.com/codecrafters-io/cli/releases/download/v${version}/v${version}_linux_amd64.tar.gz";
+    sha256 = "sha256-WJE2o4tvq9WzfWIYXXJ2gIFf07F5R7CWiBqBfnNkuAo=";
   };
 
   unpackPhase = ''
