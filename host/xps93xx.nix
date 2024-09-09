49ea094e75c1198471459561367aa9e2f84288a2
miracoly <68049792+miracoly@users.noreply.github.com>
2024-09-09 22:26:09 +0200

    chore: increase kernel version

--- host/xps93xx.nix
+++ host/xps93xx.nix
@@ -1,28 +1,36 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
   # Kernel
-  boot.kernelPackages = pkgs.linuxPackages_6_9;
+  boot = {
+    kernelPackages = pkgs.linuxPackages_6_10;
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
 
-  # Host specific
-  services.xserver.videoDrivers = [ "modesetting" ];
-  hardware.opengl.extraPackages = with pkgs; [ intel-media-driver ];
-  services.libinput.enable = true;
+    # Setup keyfile
+    initrd = {
+      secrets = {
+        "/crypto_keyfile.bin" = null;
+      };
 
-  hardware.bluetooth.enable = true;
-  services.blueman.enable = true;
+      # Enable swap on luks
+      luks.devices."luks-eda0301b-844e-4dae-98c0-1bb2bd8fc293".device = "/dev/disk/by-uuid/eda0301b-844e-4dae-98c0-1bb2bd8fc293";
+      luks.devices."luks-eda0301b-844e-4dae-98c0-1bb2bd8fc293".keyFile = "/crypto_keyfile.bin";
+    };
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # Setup keyfile
-  boot.initrd.secrets = {
-    "/crypto_keyfile.bin" = null;
   };
 
-  # Enable swap on luks
-  boot.initrd.luks.devices."luks-eda0301b-844e-4dae-98c0-1bb2bd8fc293".device = "/dev/disk/by-uuid/eda0301b-844e-4dae-98c0-1bb2bd8fc293";
-  boot.initrd.luks.devices."luks-eda0301b-844e-4dae-98c0-1bb2bd8fc293".keyFile = "/crypto_keyfile.bin";
+  # Host specific
+  services = {
+    xserver.videoDrivers = [ "modesetting" ];
+    blueman.enable = true;
+    libinput.enable = true;
+  };
 
+  hardware = {
+    opengl.extraPackages = with pkgs; [ intel-media-driver ];
+    bluetooth.enable = true;
+  };
+
+  # Bootloader.
   networking.hostName = "miras-xps-93xx"; # Define your hostname.
 }
