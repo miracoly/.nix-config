49e5a1820555ebf3b1303e49ce742541bf3dff45
miracoly <68049792+miracoly@users.noreply.github.com>
2024-11-11 08:06:16 +0100

    refactor(host): reorder xps9730 host config

--- host/xps9730.nix
+++ host/xps9730.nix
@@ -1,30 +1,40 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   # Kernel
-  boot.kernelPackages = pkgs.linuxPackages_6_10;
+  boot = {
+    kernelPackages = pkgs.linuxPackages_6_10;
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+    #  set a custom kernel parameter to 'thunderbolt.host_reset=false
+    kernelParams = [ "thunderbolt.host_reset=false" ];
 
-  # Setup keyfile
-  boot.initrd.secrets = {
-    "/crypto_keyfile.bin" = null;
+    # Bootloader.
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+
+    initrd = {
+      # Setup keyfile
+      secrets = {
+        "/crypto_keyfile.bin" = null;
+      };
+
+      # Enable swap on luks
+      luks.devices."luks-d32204ca-bfa7-4f5c-99a7-8cf46dc8d5ff".device = "/dev/disk/by-uuid/d32204ca-bfa7-4f5c-99a7-8cf46dc8d5ff";
+      luks.devices."luks-d32204ca-bfa7-4f5c-99a7-8cf46dc8d5ff".keyFile = "/crypto_keyfile.bin";
+    };
   };
-
-  #  set a custom kernel parameter to 'thunderbolt.host_reset=false
-  boot.kernelParams = [ "thunderbolt.host_reset=false" ];
-
-  # Enable swap on luks
-  boot.initrd.luks.devices."luks-d32204ca-bfa7-4f5c-99a7-8cf46dc8d5ff".device = "/dev/disk/by-uuid/d32204ca-bfa7-4f5c-99a7-8cf46dc8d5ff";
-  boot.initrd.luks.devices."luks-d32204ca-bfa7-4f5c-99a7-8cf46dc8d5ff".keyFile = "/crypto_keyfile.bin";
 
   networking.hostName = "miras-xps-9730"; # Define your hostname.
 
-  hardware.bluetooth.enable = true;
-  services.blueman.enable = true;
-  services.libinput.enable = true;
+  services = {
+    blueman.enable = true;
+    libinput.enable = true;
+  };
 
-  hardware.opengl.extraPackages = with pkgs; [ intel-media-driver ];
+  hardware = {
+    bluetooth.enable = true;
+    opengl.extraPackages = with pkgs; [ intel-media-driver ];
+  };
 }
