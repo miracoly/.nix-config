df59f996b55b7918e087b3021322c2fea927d185
miracoly <68049792+miracoly@users.noreply.github.com>
2024-12-25 18:33:41 +0100

    fix(nvim): make format keybinding work

--- modules/home-manager/nixvim/lua/lsp/onAttach.lua
+++ modules/home-manager/nixvim/lua/lsp/onAttach.lua
@@ -1,13 +1,13 @@
--- The following two autocommands are used to highlight references of the
--- word under your cursor when your cursor rests there for a little while.
---    See `:help CursorHold` for information about when this is executed
---
--- When you move your cursor, the highlights will be cleared (the second autocommand).
 (function(client, bufnr, vim)
   if not client then
     return
   end
 
+  -- The following two autocommands are used to highlight references of the
+  -- word under your cursor when your cursor rests there for a little while.
+  --    See `:help CursorHold` for information about when this is executed
+  --
+  -- When you move your cursor, the highlights will be cleared (the second autocommand).
   if client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
     local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
     vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
@@ -36,15 +36,17 @@
     vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
   end
 
+  -- Add a keybinding to toggle the display of inlay hints
   if client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
     map("<leader>th", function()
       vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }))
     end, "[T]oggle Inlay [H]ints")
   end
 
+  -- Add a keybinding to format the current buffer
   if client.supports_method("textDocument/formatting") then
     map("<leader>f", function()
-      vim.lsp.buf.format({ bufnr = bufnr, id = client.id })
+      vim.lsp.buf.format({ bufnr = bufnr })
     end, "[F]ormat")
   end
 
