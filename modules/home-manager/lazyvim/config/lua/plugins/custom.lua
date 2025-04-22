f791846b5c4ecb10a8b2edadb8f13606ca4a69c8
miracoly <68049792+miracoly@users.noreply.github.com>
2025-04-22 14:47:29 +0200

    feat(lazyvim): add neotest-jest

--- modules/home-manager/lazyvim/config/lua/plugins/custom.lua
+++ modules/home-manager/lazyvim/config/lua/plugins/custom.lua
@@ -9,4 +9,22 @@ return {
       },
     },
   },
+  { "nvim-neotest/neotest-jest" },
+  {
+    "nvim-neotest/neotest",
+    opts = function(_, opts)
+      opts.adapters = opts.adapters or {}
+      table.insert(
+        opts.adapters,
+        require("neotest-jest")({
+          jestCommand = "pnpm test --",
+          jestConfigFile = "custom.jest.config.ts",
+          env = { CI = true },
+          cwd = function()
+            return vim.fn.getcwd()
+          end,
+        })
+      )
+    end,
+  },
 }
