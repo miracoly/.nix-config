4518a7aa9c6ea55b27296cd12260c9072ef912fa
miracoly <68049792+miracoly@users.noreply.github.com>
2025-05-11 20:22:47 +0200

    feat(lazyvim): configure neotest for vitest

--- modules/home-manager/lazyvim/config/lua/plugins/custom.lua
+++ modules/home-manager/lazyvim/config/lua/plugins/custom.lua
@@ -1,3 +1,18 @@
+local function find_vitest_root()
+  local root = vim.fn.expand("%:p:h")
+  while root ~= "/" do
+    if
+      vim.fn.filereadable(root .. "/vitest.config.ts") == 1
+      or vim.fn.filereadable(root .. "/vitest.config.js") == 1
+      or vim.fn.filereadable(root .. "/package.json") == 1
+    then
+      return root
+    end
+    root = vim.fn.fnamemodify(root, ":h")
+  end
+  return vim.fn.getcwd() -- fallback
+end
+
 return {
   { "williamboman/mason.nvim", enabled = false },
   {
@@ -6,6 +21,19 @@ return {
     opts = {
       formatters_by_ft = {
         nix = { "alejandra" },
+      },
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "marilari88/neotest-vitest",
+    },
+    opts = {
+      adapters = {
+        ["neotest-vitest"] = {
+          cwd = find_vitest_root,
+        },
       },
     },
   },
