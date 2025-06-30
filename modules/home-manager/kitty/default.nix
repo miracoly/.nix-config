dc7eb8b73ed8c6d292e68acff97842a57c7e5af9
miracoly <68049792+miracoly@users.noreply.github.com>
2025-06-30 15:23:24 +0200

    refactor: remove unmapping of kitty shortcuts
    Those unmappings were necessary for my initial nvim shortcuts to work.
    Using lazyvim, they are obsolete.

--- modules/home-manager/kitty/default.nix
+++ modules/home-manager/kitty/default.nix
@@ -7,10 +7,10 @@ _: {
     # theme = "Monokai Pro (Filter Machine)";
     keybindings = {
       # unmap conflicts with neovim
-      "ctrl+shift+b" = "no_op";
-      "ctrl+shift+f" = "no_op";
-      "ctrl+shift+n" = "no_op";
-      "ctrl+shift+w" = "no_op";
+      # "ctrl+shift+b" = "no_op";
+      # "ctrl+shift+f" = "no_op";
+      # "ctrl+shift+n" = "no_op";
+      # "ctrl+shift+w" = "no_op";
     };
   };
 }
