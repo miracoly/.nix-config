92c934c2b71eff709fed1a510fb07633300c5065
miracoly <68049792+miracoly@users.noreply.github.com>
2025-07-08 23:05:53 +0200

    feat(espanso): add neovim copilot snippet

--- modules/home-manager/espanso/default.nix
+++ modules/home-manager/espanso/default.nix
@@ -2,13 +2,13 @@ _: {
   services.espanso = {
     enable = true;
     matches = {
-      ai-prompts = {
+      ai = {
         matches = [
           {
             trigger = ":ai.test.jest";
             label = "AI > Test > Jest";
             form = ''
-              Please create Jest tests for the file [[filename]].
+              Please create Jest tests for the file `[[filename]]`.
               Follow best practices and conventions.
               Do not add any comments.
               Use `const user = userEvent.setup();` for interacting with components if necessary.
@@ -18,6 +18,15 @@ _: {
 
               Mock the following imports. Do not mock anything else.
               -
+            '';
+          }
+          {
+            trigger = ":ai.nvim.allfiles";
+            label = "AI > Neovim > Include All Files";
+            force_mode = "clipboard";
+            paste_shortcut = "CTRL";
+            replace = ''
+              > #files:
             '';
           }
         ];
